558bc3fe8b7cba35f5691b2750da833bca7d2d68
Matt Mullen <mhmullen@icloud.com>
2023-11-05 15:07:49 -0800

    add charChoice function

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ var upcase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q
 var symbols = ["!", "@", "#", "%", "^", "&", "*", "(",")", "_", "+", "=","-","< ",">","?","/"]
 var numbs = [1,2,3,4,5,6,7,8,9,0,]
   
-//var upcase = lwcase.join("','").toUpperCase().split(); takes the lwcase array and converts it into uppercase string
-// it doesn't work
+//var upcase = lwcase.join("','").toUpperCase().split(); takes the lwcase array and converts it into one uppercase string
 
-console.log(lwcase);
-console.log(upcase);
-console.log(symbols);
+var charChoice = function () {
+  var charChoice = window.Prompt("Choose a password length (8 to 128 characters :");
+    return;
+} 
+var 
 
 
 // Write password to the #password input
